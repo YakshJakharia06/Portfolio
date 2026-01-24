3ca6ccde88799802582f99dafd377029b050fd68
YakshJakharia06 <jakhariayaksh@gmail.com>
2026-01-24 12:12:17 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,13 +17,10 @@ const Navbar = ({ onDownloadResume }) => {
 
   return (
     <div>
-      <nav className="fixed top-0 w-full z-50 bg-slate-950/80 backdrop-blur-md border-b border-slate-800">
-        <div className="max-w-6xl mx-auto px-4 h-16 flex items-center justify-between">
+      <nav className="fixed top-0 w-full not-sm:w-93.5 z-50 bg-slate-950/80 backdrop-blur-md border-b border-slate-800">
+        <div className="max-w-6xl mx-auto px-4 h-16 flex items-center justify-between overflow-x-hidden">
           {/* Enhanced Logo Design */}
-          <div
-            onClick={() => scrollTo('home')}
-            className="flex items-center gap-1.5 cursor-pointer group"
-          >
+          <div onClick={() => scrollTo('home')} className="flex items-center gap-1.5 cursor-pointer group">
             <div className="w-8 h-8 bg-linear-to-br from-blue-500 to-emerald-500 rounded-lg flex items-center justify-center font-mono font-bold text-white shadow-lg shadow-blue-500/20 group-hover:scale-110 transition-transform">
               Y
             </div>
@@ -35,48 +32,35 @@ const Navbar = ({ onDownloadResume }) => {
           {/* Desktop Nav */}
           <div className="hidden md:flex gap-8 text-sm font-medium ">
             {['home', 'about', 'projects', 'contact'].map((item) => (
-              <button
-                key={item}
-                onClick={() => scrollTo(item)}
-                className={`capitalize transition-colors hover:text-blue-400 ${activeSection === item ? 'text-blue-400' : 'text-slate-400'}`}
-              >
+              <button key={item} onClick={() => scrollTo(item)} className={`capitalize transition-colors hover:text-blue-400 ${activeSection === item ? 'text-blue-400' : 'text-slate-400'}`} >
                 {item}
               </button>
 
             ))}
-            <button
-              onClick={onDownloadResume}
-              className="px-4 py-2 bg-blue-600 text-white rounded-lg text-xs font-bold"
-            >
+            <button onClick={onDownloadResume} className="px-4 py-2 bg-blue-600 text-white rounded-lg text-xs font-bold">
               RESUME PDF
             </button>
           </div>
 
-          <button className="md:hidden " onClick={() => setIsMenuOpen(!isMenuOpen)}>
-            {isMenuOpen ? <X /> : <Menu />}
+          <button className="md:hidden text-slate-100" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            {isMenuOpen ? <X size={28} /> : <Menu size={28} />}
           </button>
 
         </div>
 
         {/* Mobile Nav */}
         {isMenuOpen && (
-          <div className="md:hidden bg-slate-900 p-4 border-b border-slate-800 animate-in slide-in-from-top duration-300">
+          <div className="md:hidden w-full mt-15 bg-slate-900 p-4 border-b border-slate-800 animate-in slide-in-from-top duration-300 fixed top-0">
             {['home', 'about', 'projects', 'contact'].map((item) => (
-              <button
-                key={item}
-                onClick={() => scrollTo(item)}
-                className="block w-full text-left py-3 capitalize text-slate-300 hover:text-white"
-              >
+              <button key={item} onClick={() => scrollTo(item)} className="block w-full text-left py-3 capitalize text-slate-300 hover:text-white">
                 {item}
               </button>
             ))}
-            <button
-              onClick={() => {
+            <button onClick={() => {
                 onDownloadResume();
-                setIsMenuOpen(false); 
+                setIsMenuOpen(false);
               }}
-              className="mt-4 w-full px-4 py-3 bg-blue-600 text-white rounded-lg text-sm font-bold text-center"
-            >
+              className="mt-4 w-fit px-4 py-3 bg-blue-600 text-white rounded-lg text-sm font-bold text-center">
               RESUME PDF
             </button>
           </div>
